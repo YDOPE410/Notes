95b9096505c2c4247f736b665f2f0b711226bde0
SIMCORD\d.shekhovtsov <d.shekhovtsov@simcord.com>
2019-09-17 16:39:11 +0300

    init 2

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Welcome from './Welcome';
+
+
+function App() {
+  return(
+    <div>
+     
+    </div>
+    
+  )
+}
+
+export default App;
---
+++ src/Welcome.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+function Welcome(props) {
+    return (
+        <h1>Welcome, {props.name}</h1>
+    )
+}
+
+export default Welcome;
